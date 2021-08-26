450b8917ba40f20d6f2b2f0e087e405192967e37
H M <Manjunath.HM@lumen.com>
2021-08-26 11:33:32 +0530

    Initial Commit

---
+++ src/test/java/org/fourstack/customerdata/CustomerDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fourstack.customerdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
