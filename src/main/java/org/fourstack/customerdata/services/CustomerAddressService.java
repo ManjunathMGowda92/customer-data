4b2fa1c88c7f04cd7d496f746590f1b0cca0fdbf
H M <Manjunath.HM@lumen.com>
2021-08-29 21:16:03 +0530

    Added service class for Address

--- src/main/java/org/fourstack/customerdata/services/CustomerAddressService.java
+++ src/main/java/org/fourstack/customerdata/services/CustomerAddressService.java
@@ -1,10 +1,11 @@
 package org.fourstack.customerdata.services;
 
 import org.fourstack.customerdata.model.request.AddressRequest;
+import org.fourstack.customerdata.model.response.CustomerDataResponse;
 
 public interface CustomerAddressService {
 
-    void addCustomerAddress(String accountNum, AddressRequest address);
+    CustomerDataResponse addCustomerAddress(String accountNum, AddressRequest address);
 
-
+    CustomerDataResponse updateCustomerAddress(String accountNum, AddressRequest address);
 }
---
+++ src/main/java/org/fourstack/customerdata/services/CustomerAddressServiceImpl.java
@@ -0,0 +1,33 @@
+package org.fourstack.customerdata.services;
+
+import org.fourstack.customerdata.model.request.AddressRequest;
+import org.fourstack.customerdata.model.response.CustomerDataResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static org.fourstack.customerdata.constants.CustomerConstants.ADDRESS_CREATED;
+import static org.fourstack.customerdata.constants.CustomerConstants.ADDRESS_UPDATED;
+
+@Service
+public class CustomerAddressServiceImpl implements  CustomerAddressService {
+
+    @Override
+    public CustomerDataResponse addCustomerAddress(String accountNum, AddressRequest address) {
+        return new CustomerDataResponse(HttpStatus.CREATED.value(),
+                HttpStatus.CREATED.name(),
+                ADDRESS_CREATED,
+                LocalDateTime.now(),
+                "UNKNOWN");
+    }
+
+    @Override
+    public CustomerDataResponse updateCustomerAddress(String accountNum, AddressRequest address) {
+        return new CustomerDataResponse(HttpStatus.OK.value(),
+                HttpStatus.OK.name(),
+                ADDRESS_UPDATED,
+                LocalDateTime.now(),
+                "UNKNOWN");
+    }
+}
